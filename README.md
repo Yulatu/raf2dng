0a8a75d550df4bd2ed383c13a5737b188e21a783
TelAntiNomy <yulatu@163.com>
2020-06-21 10:27:00 +0800

    changed code by OO style; add sample images; still problems on CFAHeader cause I haven't find the detailed file format description.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Main Idea
+
+The project is unfinished now.
+
+The project is meant to transfer Fujifilm's .RAF  raw file into .DNG file, as well as display EXIF information.
+
+My final aim including adding PyQt and Pypi inside it.
+
+# Reference
+
+[Usage of struct](https://docs.python.org/3/library/struct.html)
+
+[File format of RAF](http://fileformats.archiveteam.org/wiki/Fujifilm_RAF)
+
+[File format of CFA]()
+
+[File format of DNG]()
+
+
+
--- rawScript.py
+++ rawScript.py
@@ -1,31 +1,67 @@
 import struct
 
-FilePath = './img/test.RAF'
+file_path = './img/test_uncompressed.RAF'
+export_path = './dng/test.dng'
+
+
+class RAFHeader:
+    def __init__(self, filename):
+        with open(filename, 'rb') as f:
+            self.type_string = struct.unpack('>16s', f.read(16))[0]
+            self.format_ver = struct.unpack('>4s', f.read(4))[0]
+            self.camera_id = struct.unpack('>8s', f.read(8))[0]
+            self.camera_str = struct.unpack('>32s', f.read(32))[0].decode("utf_8")
+            self.offset_ver = struct.unpack('>4s', f.read(4))[0]
+            self.offset_unk = struct.unpack('>20s', f.read(20))[0].decode("utf_8")
+            self.offset_jpg_offset = struct.unpack('>1i', f.read(4))[0]
+            self.offset_jpg_length = struct.unpack('>1i', f.read(4))[0]
+            self.offset_CFA_header_offset = struct.unpack('>1i', f.read(4))[0]
+            self.offset_CFA_header_length = struct.unpack('>1i', f.read(4))[0]
+            self.offset_CFA_offset = struct.unpack('>1i', f.read(4))[0]
+            self.offset_CFA_length = struct.unpack('>1i', f.read(4))[0]
+
+
+class JPEG:
+    def __init__(self, filename, offset, length):
+        with open(filename, 'rb') as f:
+            f.seek(offset)
+
+
+class CFAHeader:
+    def __init__(self, filename, offset, length):
+        self.data = []
+        with open(filename, 'rb') as f:
+            f.seek(offset)
+            self.count = struct.unpack('>1i', f.read(4))[0]
+            for record in range(self.count):
+                tag = struct.unpack('>1h', f.read(2))[0]
+                size = struct.unpack('>1h', f.read(2))[0]
+                data = struct.unpack('>'+str(int(size/4))+'i', f.read(size))[0]
+                self.data.append({"id": tag, "size": size, "data": data})
+
+
+class CFA:
+    def __init__(self, filename, offset, length):
+        with open(filename, 'rb') as f:
+            f.seek(offset)
 
 
 class RAF:
     def __init__(self, filename):
-        with open(filename, 'rb') as f:
-            self.type_String = struct.unpack('>16s', f.read(16))[0]
-            self.Format_Ver = struct.unpack('>4s', f.read(4))[0]
-            self.Camera_Id = struct.unpack('>8s', f.read(8))[0]
-            self.Camera_Str = struct.unpack('>32s', f.read(32))[0]
-            self.offset_Ver = struct.unpack('>4s', f.read(4))[0]
-            self.offset_Unk = struct.unpack('>20s', f.read(20))[0]
-            self.offset_Jpg_Offset = struct.unpack('>4b', f.read(4))[0]
-            # self.offset_Jpg_Length = struct.unpack('>4b', f.read(4))[0]
-            # self.offset_CFA_Header_Offset = struct.unpack('>4b', f.read(4))[0]
-            # self.offset_CFA_Header_Length = struct.unpack('>4b', f.read(4))[0]
-            # self.offset_CFA_Offset = struct.unpack('>4b', f.read(4))[0]
-            # self.offset_CFA_Length = struct.unpack('>4b', f.read(4))[0]
+        self.header = RAFHeader(filename)
+        # self.jpg = JPEG(filename, self.header.offset_jpg_offset, self.header.offset_jpg_length)
+        self.CFA_header = CFAHeader(filename, self.header.offset_CFA_header_offset, self.header.offset_CFA_header_length)
+        # self.CFA = CFA(filename, self.header.offset_CFA_offset, self.header.offset_CFA_length)
 
+    def export_EXIF(self):
+        pass
 
-    def display_EXIF(self):
+    def export_DNG(self, path):
         pass
 
 
 if __name__ == '__main__':
-    obj = RAF(FilePath)
+    obj = RAF(file_path)
     pass
     # with open(FilePath, "rb") as f:
     #     # 循环读取一张图片，一次性读取1024个字节
